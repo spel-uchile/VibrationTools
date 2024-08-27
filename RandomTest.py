58f65944716bb4c9f2f6721beb88a2a45b75cdc3
EliasObreque <elias.obreque@ug.uchile.cl>
2024-08-27 17:09:10 -0400

    text data on natural frequency

--- RandomTest.py
+++ RandomTest.py
@@ -13,12 +13,12 @@ from tools.math_tools import get_psd_from_cos
 M = 4096  # number of samples in the generated signal in time
 s_max = 2 ** 14 - 1   # Max level of 14-bit DAC is 16,383
 s_zero = 2 ** 14 / 2  # "Zero" of waveform is 8,192
-fsamp = 5000  # Hz
+fsamp = 6000  # Hz
 signalDuration = 60
 
 # Tom Irvine Method
-# get_acc_from_psd(M, s_max, s_zero, fsamp, signalDuration)
+get_acc_from_psd(M, s_max, s_zero, fsamp, signalDuration)
 
 
 # Cos method
-get_psd_from_cos(M, s_max, s_zero, fsamp, signalDuration)
+# get_psd_from_cos(M, s_max, s_zero, fsamp, signalDuration)
